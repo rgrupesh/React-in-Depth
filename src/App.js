969aa33358cad14b599ecffbb442ee5d9ee7fbe0
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-10-15 10:46:49 +0545

    conditionals in render

--- src/App.js
+++ src/App.js
@@ -43,32 +43,38 @@ class App extends Component {
       cursor: "pointer",
       border: "1px solid brown",
     };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            click={this.switchNameHandler.bind(this, "Rupessshhh")}
+            changed={this.nameChangeHandler}
+          >
+            Loves Football
+          </Person>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hello World!</h1>
         <button style={style} onClick={this.toogleNameHandler}>
           Toogle Person
         </button>
-        {this.state.showPersons === true ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-            />
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              click={this.switchNameHandler.bind(this, "Rupessshhh")}
-              changed={this.nameChangeHandler}
-            >
-              Loves Football
-            </Person>
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
